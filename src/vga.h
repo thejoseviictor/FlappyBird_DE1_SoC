9ca705badf39781188ed2f1e4e14c958ba17a08d
José Victor <143978440+thejoseviictor@users.noreply.github.com>
2024-11-18 23:46:44 -0300

    "sprite_drawing" instruction and new colors

--- src/vga.h
+++ src/vga.h
@@ -12,14 +12,22 @@
 #define COLOR_MAGENTA 0x1C7
 #define COLOR_WHITE 0x1FF
 #define COLOR_BLACK 0x00
+#define COLOR_DARK_SILVER 0x1B6
 #define COLOR_SILVER 0x16D
 #define COLOR_GRAY 0xDB
+#define COLOR_SCARLET 0x0D
+#define COLOR_ORANGE 0x5E
+#define COLOR_GOLDEN_YELLOW 0x6F
+#define COLOR_VIVID_RED 0x4F
+#define COLOR_BURGUNDY 0x4D
+#define COLOR_ALPHA 0x1FE
 
 // Instruções do VGA:
 extern void physical_to_virtual();                                                         // Syscalls "open" e "mmap2"
 extern void unmap_memory();                                                                // Syscall "munmap"
 extern void background_color(int color);                                                   // Instrução WBR;
 extern void background_drawing(int row, int column, int color);                            // Instrução WBM;
+extern void sprite_drawing(int row, int column, int color);                                // Instrução WSM;
 extern void background_block_erase(int row, int column);                                   // Instrução WBM;
 extern void draw_polygon(int address, int row, int column, int size, int color, int type); // Instrução DP;
 extern void set_sprite(int address, int offset, int row, int column, int sp);              // Instrução WBR;
