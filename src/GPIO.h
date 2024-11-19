9e4a97af5df82ace8f0d2bd96b821bb0ae06f853
José Victor <143978440+thejoseviictor@users.noreply.github.com>
2024-11-18 23:44:36 -0300

    Including "GPIO" mapping and keys

---
+++ src/GPIO.h
@@ -0,0 +1,13 @@
+// Funções relacionadas aos componentes de entrada, ligados ao "GPIO" do DE1-SoC
+
+#ifndef GPIO_H
+#define GPIO_H
+
+// Chamadas das Bibliotecas:
+#include "game_features.h"
+
+// Cabeçalho das Funções:
+int GPIO_mapping(void);
+int read_keys(void);
+
+#endif // GPIO_H
