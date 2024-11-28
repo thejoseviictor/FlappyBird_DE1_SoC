c7a1af23dae1aae9db3fb0b9bfa22378c4bcbc9a
José Victor <143978440+thejoseviictor@users.noreply.github.com>
2024-11-27 22:51:13 -0300

    Refactoring names and removing "buffer_overflow"

--- src/game_features.h
+++ src/game_features.h
@@ -33,16 +33,16 @@ extern int on_pause;
 // Cabeçalho das Funções:
 int set_game_background(void);
 int clean_screen(void);
-int show_background(void);
+int render_background(void);
 int show_lives(void);
 int set_players(void);
+void* render_clouds(void* arg);
 int set_pipe(void);
 int sp_row_to_bg_row(int sp_row);
 int sp_column_to_bg_column(int sp_column);
 int bg_row_to_sp_row(int bg_row);
 int bg_column_to_sp_column(int bg_column);
 int player_pipe_collision(int pipe_column, int pipe_disabled_row);
-int player_ground_collision(void);
 void* show_score_and_gameover(void* arg);
 
 #endif // GAME_FEATURES_H
